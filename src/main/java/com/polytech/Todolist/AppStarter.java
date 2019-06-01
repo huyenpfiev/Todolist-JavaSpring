7bcfadea61b759f08c5442b7e41465cf86ee062f
DINH Thi-Thanh-Huyen <caydinhnong2009@gmail.com>
2019-06-02 00:29:17 +0200

    changed

--- src/main/java/com/polytech/Todolist/AppStarter.java
+++ src/main/java/com/polytech/Todolist/AppStarter.java
@@ -1,10 +1,14 @@
 package com.polytech.Todolist;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class AppStarter {
     public static void main(String[] args) {
         SpringApplication.run(AppStarter.class);
